93d2c73cae29ed318f6c80e6dbc79fb364fea44c
R M weerathunga <randewmegha98@gmail.com>
2026-07-27 21:31:13 +0530

    fix(api): update default API URL to use secure HTTPS endpoint

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -2,4 +2,4 @@ const configuredUrl = process.env.EXPO_PUBLIC_API_URL?.trim();
 
 // 10.0.2.2 points from the Android emulator to the development computer.
 // Physical devices must set EXPO_PUBLIC_API_URL to the computer's LAN IP.
-export const API_URL = (configuredUrl || "http://192.168.177.1:5001/api").replace(/\/$/, "");
+export const API_URL = (configuredUrl || "https://recipenest-gu9v.onrender.com/api").replace(/\/$/, "");
